8393fea93bc628d4f267af30b4b3171ee82b91b2
Dream --- <Dream --->
2017-04-14 19:28:16 -0300

    Added GET todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var {ObjectID} = require("mongodb");
 var {mongoose} = require("./db/mongoose.js");
 var {Todo}=require("./models/todo");
 var {User}=require("./models/user");
@@ -34,6 +34,17 @@ app.get("/todos",(req,res)=>{
     });
 });
 
+app.get("/todos/:id",(req,res)=>{
+   var id= req.params.id;
+   if(!ObjectID.isValid(id)) {
+    return res.status(400).send();
+}
+    Todo.findById(id).then((todo)=>{
+        res.send({todo})
+    }, (e)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(3000, ()=>{
     console.log("Arrancando");

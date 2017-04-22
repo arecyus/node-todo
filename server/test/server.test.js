dc64b742681730fb8a877fec339f2a4ad1197dee
Dream --- <Dream --->
2017-04-22 08:53:04 -0300

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -288,3 +288,22 @@ describe("POST /users/login", ()=>{
             });
    });
 });
+
+describe("DELETE /users/token/me",()=>{
+   it("Deberia eliminar el auth token al desloguearse",(done)=>{
+       request(app)
+           .delete("/users/me/token")
+           .set("x-auth",users[0].tokens[0].token)
+           .expect(200)
+           .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+
+            User.findById(users[0]._id).then((user)=>{
+               expect(user.tokens.length).toBe(0);
+               done();
+            }).catch((e)=>done(e));
+           });
+   })
+});

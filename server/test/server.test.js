1d02092fddb2b143c76824f279ee579892f47a33
Dream --- <Dream --->
2017-04-22 08:36:57 -0300

    Add tests for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -212,7 +212,9 @@ describe("POST /users", () =>{
                 expect(user).toExist();
                 expect(user.password).toNotBe(password);
                 done();
-            });
+            }).catch((e)=>{
+                   done(e);
+               });
            });
 
     });
@@ -237,3 +239,52 @@ describe("POST /users", () =>{
     });
 
 })
+
+describe("POST /users/login", ()=>{
+   it("Deberia loguear user y retornar el auth token",(done)=>{
+      request(app)
+          .post("/users/login")
+          .send({
+              email:users[1].email,
+              password:users[1].password
+          })
+          .expect(200)
+          .expect((res)=>{
+            expect(res.headers["x-auth"]).toExist();
+          })
+          .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+            User.findById(users[1]._id).then((user)=>{
+               expect(user.tokens[0]).toInclude({
+                  access: "auth",
+                   token: res.headers["x-auth"]
+               });
+               done();
+            }).catch((e)=>done(e));
+          });
+   });
+
+   it("Deberia reject login invalido",(done)=>{
+        request(app)
+            .post("/users/login")
+            .send({
+                email:users[1].email,
+                password:"12345678"
+            })
+            .expect(400)
+            .expect((res)=>{
+                expect(res.headers["x-auth"]).toNotExist();
+            })
+            .end((err,res)=>{
+                if(err){
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user)=>{
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e)=>done(e));
+            });
+   });
+});
